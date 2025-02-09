abe7986fb5066472aef2d7d60d208dc6be961980
killabilla <4allsmap@gmail.com>
2025-02-09 03:41:52 +0300

    2 try fix floating point

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from decimal import Decimal, ROUND_DOWN
 
 app = FastAPI()
 
-class ExchangeRequest(BaseModel):
-    usdt_amount: float
 
 @app.get("/")
 async def root():
@@ -15,5 +13,6 @@ async def exchange(usdt_amount: float):
     if usdt_amount <= 0:
         raise HTTPException(status_code=400,detail="Сумма должна быть больше 0")
     
-    btc_amount = usdt_amount * 0.00003
-    return {"btc_amount": "{:.8f}".format(btc_amount)}
+    btc_amount = Decimal(usdt_amount) * Decimal("0.00003")
+    btc_amount = btc_amount.quantize(Decimal("0.00000001"), rounding=ROUND_DOWN)
+    return {"btc_amount": float(btc_amount)}
